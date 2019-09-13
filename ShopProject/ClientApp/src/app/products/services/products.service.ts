426209b3b948bcf7767eb88468f011b06bfc6afc
Rostyslav Skrypnyk <skrypnik-r@ukr.net>
2019-09-13 23:53:31 +0300

    configure service

--- ShopProject/ClientApp/src/app/products/services/products.service.ts
+++ ShopProject/ClientApp/src/app/products/services/products.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { IProduct } from 'src/app/interface/iproduct';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getProducts(data: PageEvent): Observable<IProduct[]> {
+    const params: HttpParams = new HttpParams({
+      fromObject: {
+        _page: String(data.pageIndex),
+        _limit: String(data.pageSize)
+      }
+    });
+
+    // ?_limit=data.pageIndex&
+    console.log(params);
+
+    return this.http.get<IProduct[]>('http://localhost:3000/products', { params });
+  }
 }
+
